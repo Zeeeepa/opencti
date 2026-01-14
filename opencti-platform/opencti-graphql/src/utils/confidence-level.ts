14f1d8873eed9f7cb6848edfb78e690429c31f17
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2026-01-14 09:18:56 +0100

    [backend] post rebase fix

--- opencti-platform/opencti-graphql/src/utils/confidence-level.ts
+++ opencti-platform/opencti-graphql/src/utils/confidence-level.ts
@@ -178,7 +178,7 @@ export const controlUserConfidenceAgainstElement = <T extends ObjectWithConfiden
   return true; // ok
 };
 
-type UpdateInput = {
+export type UpdateInput = {
   key: string | string[];
   value: string[];
 };
